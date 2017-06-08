c373af43d41df1cdc6f83aef89d892a3757828e6
Scott Savage <snsavage@gmail.com>
2017-06-08 08:20:23 -0400

    Remove test_controller

--- app/controllers/api/v1/test_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::TestController < ApplicationController
-  def index
-    respond_with JSON.parse('{"hello": "goodbye"}')
-  end
-end
--- spec/controllers/api/v1/test_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::TestController, type: :controller do
-
-end
