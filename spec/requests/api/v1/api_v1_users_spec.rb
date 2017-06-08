ad3a1375aaeacf7369bf5276fdfbed19276c82eb
Scott Savage <snsavage@gmail.com>
2017-06-08 10:47:51 -0400

    Refactor and move users_controller_spec to api_v1_users_spec

---
+++ spec/requests/api/v1/api_v1_users_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :request do
+  describe '#register' do
+    let(:url) { '/api/v1/register' }
+    let(:headers) {
+      { 'Content-Type': 'application/json' }
+    }
+
+    context 'with valid credentials' do
+      let(:params) { attributes_for(:user) }
+
+      it 'creates a new user and responds with 201' do
+        expect do
+          post url, params: {user: params}.to_json, headers: headers
+        end.to change{User.count}.by(1)
+
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'with invalid credentials' do
+      let(:params) { attributes_for(:user, password_confirmation: '') }
+
+      it 'does NOT create a new user and responds with 422' do
+        expect do
+          post url, params: {user: params}.to_json, headers: headers
+        end.to change{User.count}.by(0)
+
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+end
