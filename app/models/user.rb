64566f1f521451eccdf8e9101c375b42de1d3b8c
Scott Savage <snsavage@gmail.com>
2017-06-08 12:49:59 -0400

    Add User#jwt_payload method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
   validates :email, :first_name, presence: true
+
+  def jwt_payload
+    {id: id, email: email, first_name: first_name}
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should have_db_index(:email) }
+
+  describe '#jwt_payload' do
+    it 'returns a hash with user\' id, email, and first_name' do
+      user = create(:user)
+      payload = {
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name
+      }
+
+      expect(user.jwt_payload).to eq(payload)
+    end
+  end
 end
