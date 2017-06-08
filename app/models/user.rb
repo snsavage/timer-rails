61631478eb7887277e39562b1889c61a5cb9a397
Scott Savage <snsavage@gmail.com>
2017-06-08 11:16:36 -0400

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-
+  validates :email, uniqueness: true
+  validates :email, :first_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_secure_password }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should have_db_index(:email) }
 end
